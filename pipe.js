543b3129b0dc0e892da449700497ccce3efc03f9
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-16 16:46:46 +0100

    Add pipe

---
+++ pipe.js
@@ -0,0 +1,12 @@
+/**
+ * Similar to compose, but applies functions in the reverse order.
+ * Given functions:
+ *      f_1 :: t_1 -> t_2
+ *      f_2 :: t_2 -> t_3
+ *      ...
+ *      f_n :: t_n -> t_(n + 1)
+ * Then their composition is (f_1 o ... o f_n)(x) = f_n(...(f_1(x))...).
+ */
+const pipe = (...functions) => {
+    return x => functions.reduce((v, fn) => fn(v), x);
+};
